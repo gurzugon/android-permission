14fb6e36f39495b3cae84dfa0f3a60f7dfcc0c4a
Syahrin Suhaimi <60390413+gurzugon@users.noreply.github.com>
2021-02-17 20:45:36 +0800

    Initial commit

---
+++ WebViewClientImpl.java
@@ -0,0 +1,33 @@
+package com.example.shuttlear;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ProgressBar;
+
+
+public class WebViewClientImpl extends WebViewClient {
+
+    private Activity activity = null;
+    String ShowOrHideWebViewInitialUse = "show";
+    private WebView webView = null;
+
+    public WebViewClientImpl(Activity activity) {
+        this.activity = activity;
+    }
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+        if(url.indexOf("https://digilogbook.000webhostapp.com/") > -1 ) return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        activity.startActivity(intent);
+        return true;
+
+
+    }
+
+}
